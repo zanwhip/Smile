ce1678df844d687dc0e5fb1b88e839b98f64498e
zanwhip <hungnv.21it@vku.udn.vn>
2023-10-23 22:47:03 +0700

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
-import iconAImage from '../Assets/image/message.png';
+import iconAImage from '../Assets/image/messicon.png';
 import iconBImage from '../Assets/image/notification.png';
+import iconSearch from '../Assets/image/searchicon.png';
+import icondown from '../Assets/image/dropdown.png';
 import logo from '../Assets/image/logo.png';
-import '../css/Header.css' // Import a CSS file to apply styles
+import '../css/Header.css'; // Import a CSS file to apply styles
+import { width } from '@mui/system';
 
 function Header() {
   const [language, setLanguage] = useState('Vie');
   const [showLanguageDropdown, setShowLanguageDropdown] = useState(false);
-  const [searchQuery, setSearchQuery] = useState('');
+ 
 
   const toggleLanguageDropdown = () => {
     setShowLanguageDropdown(!showLanguageDropdown);
@@ -18,50 +21,48 @@ function Header() {
     setShowLanguageDropdown(false);
   };
 
-  const handleSearch = (event) => {
-    setSearchQuery(event.target.value);
-  };
-
   return (
     <header className="header-container">
-      <div className="logo-container">
+      <div>
         <img src={logo} alt="Logo" className="logo" />
       </div>
       <nav className="nav-container">
-        <ul className="nav-list">
-          <li><a href="/">Trang chủ</a></li>
-          <li><a href="/donate">Quyên góp</a></li>
-          <li><a href="/shop">Mua bán</a></li>
-          <li><a href="/news">Bản tin</a></li>
-        </ul>
-        <div className="language-search-icons-container">
-          <div className="language-selector" onClick={toggleLanguageDropdown}>
-            <span>{language}</span>
-            {showLanguageDropdown && (
-              <div className="language-dropdown">
-                <ul>
-                  <li onClick={() => selectLanguage('Vie')}>Vie</li>
-                  <li onClick={() => selectLanguage('Japan')}>Japan</li>
-                  <li onClick={() => selectLanguage('Tiếng Pháp')}>Tiếng Pháp</li>
-                </ul>
-              </div>
-            )}
-          </div>
-          <div className="search-box">
-            <input
-              type="text"
-              placeholder="Tìm kiếm..."
-              value={searchQuery}
-              onChange={handleSearch}
-            />
-            <i className="search-icon fa fa-search" />
-          </div>
-          <div className="icons">
-            <img src={iconAImage} alt="Icon A" className="icon" />
-            <img src={iconBImage} alt="Icon B" className="icon" />
-          </div>
-        </div>
+      <ul className="nav-list">
+        <li><a href="/" style={{ fontSize: '20px', fontWeight: 'bold', }}>Trang chủ</a></li>
+        <li><a href="/donate" style={{ fontSize: '20px', fontWeight: 'bold',color: 'black' }}>Quyên góp</a></li>
+        <li><a href="/shop" style={{ fontSize: '20px', fontWeight: 'bold', color: 'black' }}>Mua bán</a></li>
+        <li><a href="/news" style={{ fontSize: '20px', fontWeight: 'bold',color: 'black' }}>Bản tin</a></li>
+      </ul>
       </nav>
+      <div className="language-search-icons-container">
+        <div className="language-selector" onClick={toggleLanguageDropdown}>
+          <span>{language}</span>
+          {showLanguageDropdown && language === 'Vie' && (
+            <div className="language-dropdown">
+              <ul>
+                <li onClick={() => selectLanguage('Vie')}>Vie</li>
+                <li onClick={() => selectLanguage('Japan')}>Japan</li>
+                <li onClick={() => selectLanguage('French')}>French</li>
+              </ul>
+            </div>
+          )}
+        </div>
+        <img src={icondown} alt="IconDown"  style={{ width : 15, height : 15 }}/>
+        
+        </div>
+        <div class="language-search-icons-container">
+  <div class="search-box">
+    <div class="search-content">Liên hệ</div>
+  </div>
+  <div class="icon-container">
+    <img src={iconSearch} alt="search" class="searchIcon" />
+  </div>
+</div>
+      <div className="icons">
+          <img src={iconAImage} alt="Icon A" className="icon" />
+          <img src={iconBImage} alt="Icon B" className="icon" />
+        </div>
+        
     </header>
   );
 }
