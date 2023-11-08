ae12c96f1250cbf8b9970a39ca51ee3f8685c828
zanndev0511 <giangtruong051103@gmail.com>
2023-11-08 13:46:49 +0700

    tym project

--- src/features/DetailProject.js
+++ src/features/DetailProject.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import banner from '../Assets/image/detailprojectbanner.png';
 import heart from '../Assets/image/heart.png';
+import heartRed from '../Assets/image/heart_red.png';
 import time from '../Assets/image/time.png';
 import address from '../Assets/image/address.png';
 import comment from '../Assets/image/comment.png';
@@ -53,7 +54,6 @@ const form = {
 
 const box = {
   marginLeft: 10,
-
 };
 const videoContainerStyle = {
   display: 'flex',
@@ -67,7 +67,7 @@ const contentContainerStyle = {
   position: 'absolute',
   textAlign: 'center',
   width: 580,
-  
+
   paddingLeft: 15,
   paddingRight: 15,
   backgroundColor: '#EAEDF5',
@@ -117,76 +117,73 @@ const additionalInfoStyle = {
 };
 
 function DetailProject() {
+  const [heartCount, setHeartCount] = useState(0);
+
   return (
     <div>
-    
       <div style={bannerStyle}>
-      <Header/>
+        <Header />
         <img
           src={banner}
-          alt='banner'
-          className='banner'
+          alt="banner"
+          className="banner"
           style={{ width: '100%' }}
         ></img>
-        
+
         <div style={textContainerStyle}>
           <p style={textStyle2}>Chi tiết dự án </p>
         </div>
       </div>
 
-   
       <div style={formcontainer}>
-      <label style={{ color: 'black', fontSize: '34px', fontWeight: 500,  }}>
+        <label style={{ color: 'black', fontSize: '34px', fontWeight: 500 }}>
           DỰ ÁN PHÁT TRIỂN LÀNG PÀ-ONG
         </label>
         <img
           src={share}
-          alt='share'
-          className='share'
+          alt="share"
+          className="share"
           style={{ width: 30, height: 30, marginLeft: 20 }}
         ></img>
-      <br/>
-
-        
-      <img
-                src={time}
-                alt='heart'
-                className='heart'
-                style={{ width: 30, height: 30, marginRight: 20 }}
-              ></img>
-        <label
-          style={{
-            fontStyle :'italic',
-            color: 'black',
-            fontSize: '16px',
-            fontWeight: 500,
-            paddingTop: '10px',
-          }}
-        >
-         Thời gian thực hiện: 01/11/2023 ~ 01/11/2025 (2 năm){' '}
-          
-        </label>
-        <br/>
+        <br />
 
         <img
-                src={address}
-                alt='heart'
-                className='heart'
-                style={{ width: 30, height: 30, marginRight: 20 }}
-              ></img>
+          src={time}
+          alt="heart"
+          className="heart"
+          style={{ width: 30, height: 30, marginRight: 20 }}
+        ></img>
         <label
           style={{
-            fontStyle :'italic',
+            fontStyle: 'italic',
             color: 'black',
             fontSize: '16px',
             fontWeight: 500,
             paddingTop: '10px',
           }}
         >
-         Địa điểm thực hiện: cụm dân cư Pà-Căng mới{' '}
-          
+          Thời gian thực hiện: 01/11/2023 ~ 01/11/2025 (2 năm){' '}
         </label>
-      <p></p>
+        <br />
+
+        <img
+          src={address}
+          alt="heart"
+          className="heart"
+          style={{ width: 30, height: 30, marginRight: 20 }}
+        ></img>
+        <label
+          style={{
+            fontStyle: 'italic',
+            color: 'black',
+            fontSize: '16px',
+            fontWeight: 500,
+            paddingTop: '10px',
+          }}
+        >
+          Địa điểm thực hiện: cụm dân cư Pà-Căng mới{' '}
+        </label>
+        <p></p>
 
         <div style={formandbox}>
           <div style={form}>
@@ -194,21 +191,35 @@ function DetailProject() {
               <div style={videoContainerStyle}>
                 {/* Nhúng video từ YouTube */}
                 <iframe
-                  width='900'
-                  height='380'
-                  src='https://www.youtube.com/embed/jKwQ8ork7Bw'
-                  title='YouTube video player'
-                  allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+                  width="900"
+                  height="380"
+                  src="https://www.youtube.com/embed/jKwQ8ork7Bw"
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowfullscreen
                 ></iframe>
-                
               </div>
-              
             </div>
             <div style={contentContainerStyle}>
               <div style={donateTextStyle}>
-                <label style={{ fontSize : 15, fontWeight : 600, fontFamily :  'Montserrat' }}>Quyên góp</label>
-                <label style={{ fontSize : 15, fontWeight : 600, fontFamily :  'Montserrat' }}>60%</label>
+                <label
+                  style={{
+                    fontSize: 15,
+                    fontWeight: 600,
+                    fontFamily: 'Montserrat',
+                  }}
+                >
+                  Quyên góp
+                </label>
+                <label
+                  style={{
+                    fontSize: 15,
+                    fontWeight: 600,
+                    fontFamily: 'Montserrat',
+                  }}
+                >
+                  60%
+                </label>
               </div>
 
               <div style={ratingContainerStyle}>
@@ -218,39 +229,69 @@ function DetailProject() {
                 <button style={donateButtonStyle}>Đóng góp ngay</button>
               </div>
               <div style={additionalInfoStyle}>
-                <label style={{ fontSize : 15, fontWeight : 600, fontFamily :  'Montserrat' }}>Đã góp được: 6,000,000₫</label>
+                <label
+                  style={{
+                    fontSize: 15,
+                    fontWeight: 600,
+                    fontFamily: 'Montserrat',
+                  }}
+                >
+                  Đã góp được: 6,000,000₫
+                </label>
                 <div>
-                <label style={{ fontSize : 15, fontWeight : 600, fontFamily :  'Montserrat' }}>10,000,000₫</label>
+                  <label
+                    style={{
+                      fontSize: 15,
+                      fontWeight: 600,
+                      fontFamily: 'Montserrat',
+                    }}
+                  >
+                    10,000,000₫
+                  </label>
                 </div>
-                
               </div>
             </div>
           </div>
           <div style={box}>
-            
-            <label style={{ lineHeight : 2.2   }}>
-            Mỗi hộ có diện tích vườn trên 500m2. Có điện nước đầy đủ. Hơn nữa bà con có tập quán trồng trọt và chăn nuôi. Dự án nhằm xây dựng mô hình trồng trọt và chăn nuôi quy mô nhỏ trong cụm dân cư. Dự án sẽ đồng hành cùng bà con trong quá trình xây dựng, phát triển mô hình. Qua đó, chia sẻ kiến thức khoa họa và kỹ năng làm vườn, giúp bà con hiểu sâu sắc về làm kinh tế vườn và tự xây dựng được mô hình kinh tế vườn cho gia đình của mình. Kết quả là vườn nhà được sử dụng hiệu quả, mức sống được nâng cao. Hơn nữa, góp phần làm môi trường xanh sạch đẹp.
+            <label style={{ lineHeight: 2.2 }}>
+              Mỗi hộ có diện tích vườn trên 500m2. Có điện nước đầy đủ. Hơn nữa
+              bà con có tập quán trồng trọt và chăn nuôi. Dự án nhằm xây dựng mô
+              hình trồng trọt và chăn nuôi quy mô nhỏ trong cụm dân cư. Dự án sẽ
+              đồng hành cùng bà con trong quá trình xây dựng, phát triển mô
+              hình. Qua đó, chia sẻ kiến thức khoa họa và kỹ năng làm vườn, giúp
+              bà con hiểu sâu sắc về làm kinh tế vườn và tự xây dựng được mô
+              hình kinh tế vườn cho gia đình của mình. Kết quả là vườn nhà được
+              sử dụng hiệu quả, mức sống được nâng cao. Hơn nữa, góp phần làm
+              môi trường xanh sạch đẹp.
             </label>
 
-            <div>
+            <div style={{ display: 'flex' }}>
               <button
                 style={{
                   width: '170px',
                   height: '35px',
-                 backgroundColor :'white',
+                  backgroundColor: 'white',
                   border: 0,
                   margin: 10,
                   fontWeight: 700,
                   color: 'black',
                 }}
               >
-                <p style={{ fontFamily: ' Playfair Display', fontSize : 18, textDecoration :'underline' }}>Danh sách tài trợ</p>
+                <p
+                  style={{
+                    fontFamily: ' Playfair Display',
+                    fontSize: 18,
+                    textDecoration: 'underline',
+                  }}
+                >
+                  Danh sách tài trợ
+                </p>
               </button>
               <button
                 style={{
                   width: '100px',
                   height: '35px',
-                backgroundColor : 'white', 
+                  backgroundColor: 'white',
                   border: 0,
                   margin: 10,
                   fontWeight: 700,
@@ -258,32 +299,42 @@ function DetailProject() {
                   marginLeft: 40,
                 }}
               >
-                <p style={{ fontFamily: ' Playfair Display', fontSize : 18, textDecoration :'underline' }}>Kinh phí</p>
+                <p
+                  style={{
+                    fontFamily: ' Playfair Display',
+                    fontSize: 18,
+                    textDecoration: 'underline',
+                  }}
+                >
+                  Kinh phí
+                </p>
               </button>
-
-         
-              <img
-                src={heart}
-                alt='heart'
-                className='heart'
-                style={{ width: 30, height: 30, marginRight: 20 }}
-              ></img>
-             
-          
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <img
+                  src={heartCount > 0 ? heartRed : heart}
+                  alt="heart"
+                  className="heart"
+                  style={{
+                    width: 30,
+                    height: 30,
+                    marginRight: '10px',
+                    cursor: 'pointer',
+                  }}
+                  onClick={() => {
+                    let count = heartCount;
+                    setHeartCount(++count);
+                  }}
+                />
+                <span style={{ fontSize: '19px' }}>{heartCount}</span>
+              </div>
             </div>
-           
-
-           
           </div>
         </div>
-      
       </div>
-      <div style={{ marginTop : 300 }}>
-      <Footer/>
+      <div style={{ marginTop: 300 }}>
+        <Footer />
       </div>
-          
     </div>
-    
   );
 }
 
